2dfdce40a0f0af0af28df120034eeede0733ac75
Sebastian Erfort <sebastian.erfort@gmail.com>
2025-05-06 20:49:28 +0100

    recreate loadbalancer from aws/alb, tmp rm auto-scale

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -29,53 +29,73 @@ module "blog_vpc" {
   }
 }
 
-
-module "blog_autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
 
   name = "blog"
 
-  min_size            = 1
-  max_size            = 2
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
+  subnet_id              = module.blog_vpc.public_subnets[0]
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  tags = {
+    Name = "Learning Terraform"
+  }
 }
 
-module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+# module "blog_autoscaling" {
+#   source  = "terraform-aws-modules/autoscaling/aws"
+#   version = "6.5.2"
+#
+#   name = "blog"
+#
+#   min_size            = 1
+#   max_size            = 2
+#   vpc_zone_identifier = module.blog_vpc.public_subnets
+#   target_group_arns   = module.blog_alb.target_group_arns
+#   security_groups     = [module.blog_sg.security_group_id]
+#   instance_type       = var.instance_type
+#   image_id            = data.aws_ami.app_ami.id
+# }
 
-  name = "blog-alb"
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
 
   target_groups = [
     {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
       target_type      = "instance"
+      targets = {
+        blog = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   ]
 
   http_tcp_listeners = [
     {
-      port               = 80
-      protocol           = "HTTP"
+      port = 80
+      protocol = "HTTP"
       target_group_index = 0
     }
   ]
 
   tags = {
     Environment = "dev"
+    Project     = "Learning Terraform"
   }
 }
 
