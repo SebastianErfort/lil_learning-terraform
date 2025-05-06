c3317b2a203d45525d1b0610fa2de9b93d9d2b7e
Sebastian Erfort <sebastian.erfort@gmail.com>
2025-05-06 21:00:10 +0100

    fix syntax

--- main.tf
+++ main.tf
@@ -33,8 +33,6 @@ resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
 
-  name = "blog"
-
   subnet_id              = module.blog_vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
