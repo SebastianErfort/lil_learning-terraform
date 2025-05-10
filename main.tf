9c1dfa7ff26261ad45405846df4da088ac8f9e77
Sebastian Erfort <sebastian.erfort@gmail.com>
2025-05-10 18:18:22 +0100

    fix changed keys

--- main.tf
+++ main.tf
@@ -34,13 +34,14 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
   name = "blog"
+
   image_id            = data.aws_ami.app_ami.id
   instance_type       = var.instance_type
 
   min_size            = 1
   max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = [module.blog_alb.arn]
   security_groups     = [module.blog_sg.security_group_id]
 }
 
